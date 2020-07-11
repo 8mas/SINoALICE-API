47e9260a92e1915384d49b0c1b99ebccbdad8be0
see-aesatas <51927781+see-aestas@users.noreply.github.com>
2020-07-11 15:32:29 +0200

    add headers and start with login

--- api.py
+++ api.py
@@ -1,27 +1,22 @@
-import hashlib
-import random
-import time
 import requests
-import secrets
-import hmac
 import json
 import msgpack
-import crypto
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
 
+DEBUG = True
+instances = 0
 
 class API:
-    debug = True
-    instances = 0
-    URL = "https://api-sinoalice-us.pokelabo.jp/api/login"
+    URL = "https://api-sinoalice-us.pokelabo.jp"
+    key = b"***REMOVED***"
 
     def __init__(self):
         self.request_session = requests.session()
         self.request_session.verify = False
 
         # Use local proxy
-        if API.debug:
+        if DEBUG:
             print("Using proxy")
             self.request_session.proxies.update({"http": "http://127.0.0.1:8888", "https": "https://127.0.0.1:8888", })
 
@@ -29,8 +24,33 @@ class API:
 
         return ""
 
+    def _decrypt_response(self, response_content: bytes) -> dict:
+        aes = AES.new(self.key, AES.MODE_CBC, response_content[0:16])
+        pad_text = aes.decrypt(response_content[16:])
+        text = unpad(pad_text, 16)
+        data_loaded = msgpack.unpackb(text)
+        return data_loaded
+
+    def _encrypt_request(self, request_content: bytes):
+        pass
+
     # Set headers for request
-    def _prepare_request(self, request_type, resource, data):
+    def _prepare_request(self, request_type, resource, data, remove_headers=None):
+
+        # Headers
+        common_headers = {
+            "Host": "api-sinoalice-us.pokelabo.jp",
+            "User-Agent": "UnityRequest com.nexon.sinoalice 1.0.16 (OnePlus ONEPLUS A6000 Android OS 10 / API-29 (QKQ1.190716.003/2002220019))",
+            "X-Unity-Version": "2018.4.19f1",
+            "Content-Type": "application/json",
+            "Expect": "100-continue",
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip",
+            "Cookie": "TODO_Define",
+            "Content-Length": "TODO_Define",
+            "X-post-signature": "TODO_Define"
+        }
+
         return ""
 
     def _handle_response(self, response):
@@ -46,7 +66,7 @@ class API:
         response = self.request_session.get(url, params=params)
         return self._handle_response(response)
 
-    def _post(self, resource, payload: str):
+    def _post(self, resource, payload: str, remove_header=None):
         url = API.URL + resource
 
         self._prepare_request("POST", resource, payload)
@@ -59,20 +79,56 @@ class API:
     def _delete(self):
         pass
 
-    ########## Helper ##########
+    ########## API ##########
     # a bit sloppy but okay for now
+
+    def POST__api_login(self):
+        payload = {
+        "payload": {
+            "appVersion": "1.0.16",
+            "urlParam": None,
+            "deviceModel": "OnePlus ONEPLUS A6000",
+            "osType": 2,
+            "osVersion": "Android OS 10 / API-29 (QKQ1.190716.003/2002220019)",
+            "storeType": 2,
+            "graphicsDeviceId": 0,
+            "graphicsDeviceVendorId": 0,
+            "processorCount": 8,
+            "processorType": "ARM64 FP ASIMD AES",
+            "supportedRenderTargetCount": 8,
+            "supports3DTextures": True,
+            "supportsAccelerometer": True,
+            "supportsComputeShaders": True,
+            "supportsGyroscope": True,
+            "supportsImageEffects": True,
+            "supportsInstancing": True,
+            "supportsLocationService": True,
+            "supportsRenderTextures": True,
+            "supportsRenderToCubemap": True,
+            "supportsShadows": True,
+            "supportsSparseTextures": True,
+            "supportsStencil": 1,
+            "supportsVibration": True,
+            "uuid": None, # TODO check
+            "xuid": 928750150, # TODO check
+            "locale": "en_US",
+            "numericCountryCode": 826 # TODO check
+        },
+        "uuid": "***REMOVED***636a36e2c5f747fdb12e059a5f830369", # Todo generate
+        "userId": 0,
+        "sessionId": "",
+        "actionToken": None,
+        "ctag": None,
+        "actionTime": 132381034208143910
+        }
+
+        payload = json.dumps(payload)
+        self._post("/api/login", payload, remove_header={'Cookie'})
 
 
 class SigningException(Exception):
     pass
 
 if __name__ == "__main__":
-    with open("get_user_data.msg", "rb") as data_file:
-        byte_data = data_file.read()
-    crypt = crypto.RijndaelEncryptor()
-    key = b"***REMOVED***"
-    aes = AES.new(key, AES.MODE_CBC, byte_data[0:16])
-    pad_text = aes.decrypt(byte_data[16:])
-    text = unpad(pad_text, 16)
-    data_loaded = msgpack.unpackb(text)
-    print(data_loaded)
+    a = API()
+    a.POST__api_login()
