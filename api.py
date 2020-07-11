fdee8ff74f45e8c382611e8a22562a8ac59312f1
see-aesatas <51927781+see-aestas@users.noreply.github.com>
2020-07-11 20:29:34 +0200

    add decryption of response

--- api.py
+++ api.py
@@ -3,13 +3,18 @@ import json
 import msgpack
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
-
+import hmac
+import base64
+from Crypto.Hash import SHA256
 DEBUG = True
 instances = 0
 
 class API:
     URL = "https://api-sinoalice-us.pokelabo.jp"
     key = b"***REMOVED***"
+
+    hash_key = b"nKyY.is<.$zv~K5zW%GA-T&gR64.J8'Iz-I*K|}[K^kE1`;@j}SjTY<a24i0c$$X"
+    salt = b"|#=hvPI"
 
     def __init__(self):
         self.request_session = requests.session()
@@ -20,24 +25,35 @@ class API:
             print("Using proxy")
             self.request_session.proxies.update({"http": "http://127.0.0.1:8888", "https": "https://127.0.0.1:8888", })
 
-    def _generate_hmac(self, request_type, resource, timestamp, data: str):
-
-        return ""
+    def _generate_hmac(self, data: str):
+        data = data.replace(" ", "").replace("\n", "")
+        return hmac.new(self.hash_key, self.salt + data.encode(), "SHA1").hexdigest()
+    #6sgst+dfmDNDdcHwYjQ+nl08ixHUy96fxY480crKZYR4N5zmtcktY42fwOsjEZ5jHV8D5Ei8zWMkNkEvX5Bznw==
+    #M2IzMDUwZDdmY2Y3ODViOTA3OTI3N2Q1NDQyNmEwNjI0MGJjMDI1ZjRhNzA0NTk2ZGE0ZWI4OTNhM2ZlYWI0OA==
 
     def _decrypt_response(self, response_content: bytes) -> dict:
-        aes = AES.new(self.key, AES.MODE_CBC, response_content[0:16])
+        iv = response_content[0:16]
+        aes = AES.new(self.key, AES.MODE_CBC, iv)
         pad_text = aes.decrypt(response_content[16:])
         text = unpad(pad_text, 16)
         data_loaded = msgpack.unpackb(text)
         return data_loaded
 
     def _encrypt_request(self, request_content: bytes):
-        pass
+        iv = request_content[0:16] # TODO check if ok
+        aes = AES.new(self.key, AES.MODE_CBC, iv)
+        text = aes.encrypt(request_content)
+        pad_text = pad(text, 16)
+        data_loaded = msgpack.packb(pad_text)
+        return iv + data_loaded
 
-    # Set headers for request
-    def _prepare_request(self, request_type, resource, data, remove_headers=None):
+    def _prepare_request(self, request_type, resource, data, remove_header=None):
+        mac = self._generate_hmac(data) #TODO
 
-        # Headers
+        print(mac)
+        mac = base64.b64encode(mac.encode()).decode()
+        print(mac)
+
         common_headers = {
             "Host": "api-sinoalice-us.pokelabo.jp",
             "User-Agent": "UnityRequest com.nexon.sinoalice 1.0.16 (OnePlus ONEPLUS A6000 Android OS 10 / API-29 (QKQ1.190716.003/2002220019))",
@@ -47,17 +63,18 @@ class API:
             "Connection": "Keep-Alive",
             "Accept-Encoding": "gzip",
             "Cookie": "TODO_Define",
-            "Content-Length": "TODO_Define",
-            "X-post-signature": "TODO_Define"
+            "X-post-signature": f"{mac}"
         }
+        for header in remove_header:
+            common_headers.pop(header)
 
-        return ""
+        self.request_session.headers = common_headers
 
     def _handle_response(self, response):
-        response_dict = json.loads(response.content.decode())
+        decrypted_response = self._decrypt_response(response.content)
         code = response.status_code
-
-        return response_dict
+        print(decrypted_response)
+        return decrypted_response
 
     def _get(self, resource, params={}):
         url = API.URL + resource
@@ -69,7 +86,8 @@ class API:
     def _post(self, resource, payload: str, remove_header=None):
         url = API.URL + resource
 
-        self._prepare_request("POST", resource, payload)
+        self._prepare_request("POST", resource, payload, remove_header=remove_header)
+
         response = self.request_session.post(url, payload)
         return self._handle_response(response)
 
