1596db4c6fdfc95d39c0d5a23cb720d5534adafc
see-aesatas <51927781+see-aestas@users.noreply.github.com>
2020-07-15 12:43:07 +0200

    add start of oauth

--- api.py
+++ api.py
@@ -3,57 +3,99 @@ import json
 import msgpack
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
 import hmac
 import base64
-from Crypto.Hash import SHA256
+from Crypto.Hash import SHA1, MD5, SHA256
+import time
+import random
+
 DEBUG = True
 instances = 0
 
+
+def generate_nonce(length=19):
+    return ''.join([str(random.randint(0, 9)) for i in range(length)])
+
+
+def get_timestamp():
+    return time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
 class API:
     URL = "https://api-sinoalice-us.pokelabo.jp"
-    key = b"***REMOVED***"
+    crypto_key = b"***REMOVED***"
 
-    hash_key = b"nKyY.is<.$zv~K5zW%GA-T&gR64.J8'Iz-I*K|}[K^kE1`;@j}SjTY<a24i0c$$X"
-    salt = b"|#=hvPI"
 
     def __init__(self):
         self.request_session = requests.session()
         self.request_session.verify = False
+        self.consumer_key = generate_nonce(15) # Like device id
 
         # Use local proxy
         if DEBUG:
             print("Using proxy")
             self.request_session.proxies.update({"http": "http://127.0.0.1:8888", "https": "https://127.0.0.1:8888", })
 
-    def _generate_hmac(self, data: str):
-        data = data.replace(" ", "").replace("\n", "")
-        return hmac.new(self.hash_key, self.salt + data.encode(), "SHA1").hexdigest()
-    #6sgst+dfmDNDdcHwYjQ+nl08ixHUy96fxY480crKZYR4N5zmtcktY42fwOsjEZ5jHV8D5Ei8zWMkNkEvX5Bznw==
-    #M2IzMDUwZDdmY2Y3ODViOTA3OTI3N2Q1NDQyNmEwNjI0MGJjMDI1ZjRhNzA0NTk2ZGE0ZWI4OTNhM2ZlYWI0OA==
+
+    """
+    Benutzung von SHA1-RSA-PKCS 
+    DefaultSigner wird für den ersten kontakt genommen
+    Dann Authoratife für den zweiten (auth/x_uid)
+    
+    """
+    def register_new_account(self):
+        oauth_header = self._build_oauth_header()
+
+        pass
+
+    def login(self):
+        pass
+
+    def _build_oauth_header(self, body_data:bytes, new_account=False):
+
+        signatur = (httpRequset, secret, treemap)
+
+
+
+        ouathHeader = {
+            "oauth_body_hash": f"{base64.b64encode(SHA1.new(body_data).digest())}",
+            "oauth_consumer_key": f"{self.consumer_key}",
+            "oauth_nonce": f"{generate_nonce()}",
+            "oauth_signature": "application/json",
+            "oauth_signature_method": f"{'HMAC-SHA1' if new_account else 'RSA-SHA1'}",
+            "oauth_timestamp": f"{get_timestamp()}",
+            "oauth_version": "1.0"
+        }
+        pass
+
+    def _generate_signatur(self, data: bytes):
+        mac = SHA1.new(data).digest()
+        return mac
 
     def _decrypt_response(self, response_content: bytes) -> dict:
         iv = response_content[0:16]
-        aes = AES.new(self.key, AES.MODE_CBC, iv)
+        aes = AES.new(self.crypto_key, AES.MODE_CBC, iv)
         pad_text = aes.decrypt(response_content[16:])
         text = unpad(pad_text, 16)
         data_loaded = msgpack.unpackb(text)
         return data_loaded
 
     def _encrypt_request(self, request_content: bytes):
+        request_content = pad(request_content, 16)
         iv = request_content[0:16] # TODO check if ok
-        aes = AES.new(self.key, AES.MODE_CBC, iv)
+        aes = AES.new(self.crypto_key, AES.MODE_CBC, iv)
         text = aes.encrypt(request_content)
-        pad_text = pad(text, 16)
-        data_loaded = msgpack.packb(pad_text)
+        data_loaded = msgpack.packb(text)
         return iv + data_loaded
 
+
     def _prepare_request(self, request_type, resource, data, remove_header=None):
-        mac = self._generate_hmac(data) #TODO
+        data = self._encrypt_request(data.encode())
+        mac = self._generate_signatur(data)
 
-        print(mac)
-        mac = base64.b64encode(mac.encode()).decode()
-        print(mac)
-
+        exit(1)
         common_headers = {
             "Host": "api-sinoalice-us.pokelabo.jp",
             "User-Agent": "UnityRequest com.nexon.sinoalice 1.0.16 (OnePlus ONEPLUS A6000 Android OS 10 / API-29 (QKQ1.190716.003/2002220019))",
