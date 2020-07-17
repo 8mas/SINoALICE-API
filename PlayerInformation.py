e8b13a0cf0b7ff1b1cd10e446df8b6a3a858aa8a
see-aesatas <51927781+see-aestas@users.noreply.github.com>
2020-07-17 20:00:03 +0200

    add orm to playerinformation

--- PlayerInformation.py
+++ PlayerInformation.py
@@ -1,15 +1,45 @@
-from dataclasses import dataclass
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Interval, String, Integer
+from sqlalchemy import orm
+from sqlalchemy.orm import sessionmaker, session
+from sqlalchemy import create_engine
 from Crypto.PublicKey.RSA import RsaKey
+from Crypto.PublicKey import RSA
+import random
+
+Base = declarative_base()
+engine = create_engine("sqlite:///foo.db", echo=True)
+
+def generate_device_id():
+    return "==" + "".join(
+        [random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890") for _ in range(22)])
 
 
-@dataclass(unsafe_hash=True)
-class PlayerInformation:
-    server: str
-    device_id: str
-    uuid_payment: str
-    uuid_moderation: str
-    x_uid_payment: str
-    x_uid_moderation: str
-    private_key_payment: RsaKey
-    private_key_moderation: RsaKey
-    user_id: int
+class PlayerInformation(Base):
+    __tablename__ = "PlayerInformation"
+
+    server: str = Column(String)
+    device_id: str = Column(String)
+    uuid_payment: str = Column(String, primary_key=True)
+    uuid_moderation: str = Column(String)
+    x_uid_payment: str = Column(String)
+    x_uid_moderation: str = Column(String)
+    _private_key_payment: str = Column(String)
+    _private_key_moderation: str = Column(String)
+    user_id: int = Column(Integer)
+
+    private_key_payment: RsaKey = RSA.generate(512)
+    private_key_moderation: RsaKey = RSA.generate(512)
+
+    def __init__(self):
+        if self.server is None:
+            self.server = "US"
+
+        self.device_id = generate_device_id()
+        self._private_key_payment = self.private_key_payment.export_key().decode()
+        self._private_key_moderation = self.private_key_moderation.export_key().decode()
+
+    @orm.reconstructor
+    def after_construction_by_alchemy(self):
+        self.private_key_payment = RSA.import_key(self._private_key_payment)
+        self.private_key_moderation = RSA.import_key(self._private_key_moderation)
